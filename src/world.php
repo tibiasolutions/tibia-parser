9d30538a2fc8383f6767313008788b8d5d808c76
Bruno Minervino <brunominervino1993@gmail.com>
2017-01-20 14:48:51 -0200

    Now is possible get players online from world

---
+++ src/world.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Tibia Parser - Tibia.com parser informations
+ * Copyright (c) Tibia Solutions (http://tibia.solutions)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Tibia Solutions (http://tibia.solutions)
+ * @link      https://github.com/tibiasolutions/tibiaparser Project
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace TibiaParser;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+class World
+{
+    private $world = [ ];
+    
+    function __construct($world_name) {
+        $html = file_get_contents('https://secure.tibia.com/community/?subtopic=worlds&world=' . $world_name);
+    
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($html);
+    
+        $information = [ ];
+        $info = $crawler->filterXPath('//div[@class="BoxContent"]/div[@class="TableContainer"][1]/table/tr/td/div[@class="InnerTableContainer"]/table/tr');
+        foreach ($info as $row) {
+            $key  = strtolower(str_replace(' ', '_', str_replace(':', '', trim($row->firstChild->nodeValue))));
+            $value = trim($row->lastChild->nodeValue);
+    
+            if ($key === "online_record") {
+                $record = [];
+                $explode = explode(" players (on ", $value);
+                $record["players"] = $explode[0];
+                $record["date"] = str_replace(')', '', $explode[1]);
+    
+                $information[$key] = $record;
+            } else if ($key === "world_quest_titles") {
+                $quests = explode(", ", $value);
+                $information[$key] = $quests;
+            } else {
+                $information[$key] = $value;
+            }
+        }
+    
+        $this->world["information"] = $information;
+    
+        $players = [ ];
+        $online = $crawler->filterXPath('//div[@class="BoxContent"]/div[@class="TableContainer"][2]/table/tr/td/div[@class="InnerTableContainer"]/table/tr[position() > 1]');
+        foreach ($online as $row) {
+            $name = $row->childNodes[0]->nodeValue;
+            $level = $row->childNodes[1]->nodeValue;
+            $vocation = $row->childNodes[2]->nodeValue;
+            $players[] = [ 'name' => $name, 'level' => $level, 'vocation' => $vocation];
+        }
+    
+        $this->world["players"] = $players;
+    }
+    
+    public function __get($key) {
+        return $this->world[$key];
+    }
+    
+    public function __set($key, $value) {
+        $this->world[$key] = $value;
+    }
+    
+    public function __toString() {
+        return json_encode($this->player);
+    }
+}
--- tests/index.php
+++ tests/index.php
@@ -1,12 +1,18 @@
 <?php
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-use TibiaParser\Parser\Tibia;
+use TibiaParser\Player;
+use TibiaParser\World;
 
-$tibia = new Tibia();
-$player = $tibia->getPlayer('Burchan');
-if ($player['exists']) {
+$player = new Player('Kharsek');
+if ($player->exists) {
 	var_dump($player);
 } else {
 	echo 'Character does not exist.';
+}
+
+// Get Players online on Antica
+$world = new World('Antica');
+foreach ($world->players as $player) {
+  var_dump($player["name"] . ' - ' . $player["level"]);
 }
